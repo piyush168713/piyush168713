e33a3da382f752ba36ed761a4f7002889783d7fe
Vyom-Yadav <jackhammervyom@gmail.com>
2021-04-28 12:28:47 +0530

    Dereferencing of operators.

--- More_Of_OOP/Dereferencing_Operators.cpp
+++ More_Of_OOP/Dereferencing_Operators.cpp
@@ -18,9 +18,17 @@ int sum(M m){
     int S = m.*px + pm->*py;
     return S;
 }
+// '->*' Used when pointer to object and pointer to member.
+// ".*" Used when object and pointer to member
  int main(){
      M n;
+     M *pointer_to_object = &n;
+     // Pointer to a function can be useful when we have to create callback scenarios. 
+     // When we have to pass the address of a function to another function.
+     // Parenthesis are necessary.
      void (M::*pf)(int,int) = &M::set_x_y;
-     
+     (n.*pf)(10,20);
+     (pointer_to_object->*pf)(11,100); // same as above, we have just used pointers here.
+     cout<<sum(n)<<endl;
      return 0;
  }
