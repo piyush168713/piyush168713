b323b4d3f19b162720ff386d1ec558490932923d
Vyom-Yadav <jackhammervyom@gmail.com>
2021-04-29 15:32:28 +0530

    Overloading subscript operator

---
+++ More_Of_OOP/Overloading_Subscript_Operator.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class arr{
+    int a[5];
+    public: 
+        arr(int *s) {
+            for(int i =0;i<5; i++){
+                a[i] = *(s+i); // or s[i]
+            }
+        }
+        int operator[](int k){
+            return (a[k]);
+        }
+
+};
+
+int main() {
+    int x[5] = {7,8,9,4,5};
+    arr A = x; // same as arr A(x);
+    for(int i=0; i<5; i++){
+        cout<<x[i]<<"\t";
+    }
+    cout<<endl;
+    // Another way to access it.
+    for(int i=0; i<5; i++){
+        cout<<A[i]<<"\t";
+    }
+}
