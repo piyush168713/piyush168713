999a5aa8369de31134ea903f253f632da7ff5f43
Vyom-Yadav <jackhammervyom@gmail.com>
2021-05-15 11:05:45 +0530

    Exception handling

--- Exception_Handling/Try_Block_Exception.cpp
+++ Exception_Handling/Try_Block_Exception.cpp
@@ -1,20 +1,26 @@
-#include<iostream>
+#include <iostream>
 
-int main() {
-    int a,b;
-    std::cout<<"Enter values of a and b \n";
-    std::cin>>a;
-    std::cin>>b;
-    int x = a-b;
-    try {
-        if(x!=0) {
-            std::cout<<"Result"<<a/x<<'\n';
+int main()
+{
+    int a, b;
+    std::cout << "Enter values of a and b \n";
+    std::cin >> a;
+    std::cin >> b;
+    int x = a - b;
+    try
+    {
+        if (x != 0)
+        {
+            std::cout << "Result" << a / x << '\n';
         }
-        else {
+        else
+        {
             throw(x);
         }
-    } catch(int i) {
-       std::cout<<"Dividing by zero huh";
+    }
+    catch (int i)
+    {
+        std::cout << "Dividing by zero huh" << '\n';
     }
     return 0;
 }
